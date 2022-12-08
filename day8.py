68f7443f6f3add25d1fd2ff6150b2d1c685aaa0a
cekerot <carl.ekerot@recordedfuture.com>
2022-12-08 10:55:37 +0100

    Some refactoring to day 8

--- day8.py
+++ day8.py
@@ -1,46 +1,43 @@
-from itertools import takewhile
+from itertools import takewhile, product
 
 def parse():
     with open('day8.txt') as f:
         return [[int(c) for c in line.strip()] for line in f]
 
 
-def ranges(x, y, max_x, max_y):
+def grid_coords(width, border=0):
+    return product(range(border, width - border), range(border, width - border))
+
+
+def ranges(x, y, width):
     return [
         [trees[i][x] for i in range(y - 1, -1, -1)],
-        [trees[i][x] for i in range(y + 1, max_y)],
+        [trees[i][x] for i in range(y + 1, width)],
         [trees[y][i] for i in range(x - 1, -1, -1)],
-        [trees[y][i] for i in range(x + 1, max_x)],
+        [trees[y][i] for i in range(x + 1, width)],
     ]
 
 
 def part1(trees):
-    max_y = len(trees)
-    max_x = len(trees[0])
-    visible_count = max_x * 2 + max_y * 2 - 4
-    for y in range(1, max_y - 1):
-        for x in range(1, max_x - 1):
-            rs = ranges(x, y, max_x, max_y)
-            visibility = [all(h < trees[y][x] for h in r) for r in rs]
-            if any(visibility):
-                visible_count += 1
+    width = len(trees)
+    visible_count = 4 * width - 4
+    for y, x in grid_coords(width, border=1):
+        visibility = [all(h < trees[y][x] for h in r) for r in ranges(x, y, width)]
+        visible_count += 1 if any(visibility) else 0
     print(visible_count)
 
 
 def part2(trees):
-    max_y = len(trees)
-    max_x = len(trees[0])
+    width = len(trees)
     scores = []
-    for y in range(0, max_y):
-        for x in range(0, max_x):
-            score = 1
-            for r in ranges(x, y, max_x, max_y):
-                shorter = takewhile(lambda h: h < trees[y][x], r)
-                num_shorter = len(list(shorter))
-                if (num_shorter != len(r)):
-                    num_shorter +=1
-                score *= num_shorter
-            scores.append(score)
+    for y, x in grid_coords(width):
+        score = 1
+        for r in ranges(x, y, width):
+            num_shorter = sum(1 for _ in takewhile(lambda h: h < trees[y][x], r))
+            if (num_shorter != len(r)):
+                num_shorter += 1
+            score *= num_shorter
+        scores.append(score)
     print(max(scores))
 
 
